a3a7f63237c396a1dd736a11b8f386103006f907
bastiansargonhutasoit@gmail.com <bastiansargonhutasoit@gmail.com>
2022-06-07 17:42:45 +0700

    Update Header

--- pages/header.php
+++ pages/header.php
@@ -6,7 +6,7 @@
             <div class="card">
                 <div class="card-header p-3 pt-2">
                     <div class="icon icon-lg icon-shape bg-gradient-dark shadow-dark text-center border-radius-xl mt-n4 position-absolute">
-                        <i class="material-icons opacity-10">weekend</i>
+                        <i class="material-icons opacity-10">inventory</i>
                     </div>
 
                     <div class="text-end pt-1">
@@ -17,7 +17,7 @@
 
                 <hr class="dark horizontal my-0">
                 <div class="card-footer p-3">
-                    <p class="mb-0">Jumlah paket bulan <span class="text-success text-sm font-weight-bolder"><?php echo namabulan(date('m')); ?> </span></p>
+                    <p class="mb-0">Jumlah paket <span class="text-success text-sm font-weight-bolder"><?php echo namabulan(date('m'))." ".date('Y'); ?> </span></p>
                 </div>
             </div>
         </div>
@@ -26,18 +26,18 @@
             <div class="card">
                 <div class="card-header p-3 pt-2">
                     <div class="icon icon-lg icon-shape bg-gradient-primary shadow-primary text-center border-radius-xl mt-n4 position-absolute">
-                        <i class="material-icons opacity-10">person</i>
+                        <i class="material-icons opacity-10">money</i>
                     </div>
 
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">Today's Users</p>
-                        <h4 class="mb-0">2,300</h4>
+                        <p class="text-sm mb-0 text-capitalize">Pendapatan dari Paket</p>
+                        <h4 class="mb-0"><?php echo rupiah(2000000) ?></h4>
                     </div>
                 </div>
 
                 <hr class="dark horizontal my-0">
                 <div class="card-footer p-3">
-                    <p class="mb-0"><span class="text-success text-sm font-weight-bolder">+3% </span>than lask month</p>
+                    <p class="mb-0">Pendapatan paket <span class="text-success text-sm font-weight-bolder"><?php echo namabulan(date('m'))." ".date('Y') ?></span></p>
                 </div>
             </div>
         </div>
@@ -50,14 +50,14 @@
                     </div>
 
                     <div class="text-end pt-1">
-                        <p class="text-sm mb-0 text-capitalize">New Clients</p>
+                        <p class="text-sm mb-0 text-capitalize">Jumlah Tiket Terjual</p>
                         <h4 class="mb-0">3,462</h4>
                     </div>
                 </div>
 
                 <hr class="dark horizontal my-0">
                 <div class="card-footer p-3">
-                    <p class="mb-0"><span class="text-danger text-sm font-weight-bolder">-2%</span> than yesterday</p>
+                    <p class="mb-0">Pendapatan tiket <span class="text-danger text-sm font-weight-bolder"><?php echo namabulan(date('m'))." ".date('Y') ?></span></p>
                 </div>
             </div>
         </div>
